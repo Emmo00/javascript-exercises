85661e4cb43f09debc54c8b34dec38200a9e8d35
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 16:13:47 +0100

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  string = string
+    .split("")
+    .filter(
+      (element, index) =>
+        string.charCodeAt(index) > 96 && string.charCodeAt(index) < 123
+    )
+    .join("");
+  let reverseString = string.split("").reverse().join("");
+  if (string === reverseString) return true;
+  return false;
 };
 
 // Do not edit below this line
