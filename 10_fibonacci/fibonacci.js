a335f0d64655c10b02b99afd37074a3f4c34c814
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 16:25:22 +0100

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (fib_index) {
+  let prev = 1;
+  let last = 0;
+  if (fib_index < 0) return "OOPS";
+  for (let i = 0; i < fib_index; i++) {
+    let tmp = last;
+    last += prev;
+    prev = tmp;
+  }
+  return last;
 };
 
 // Do not edit below this line
