fa770d94eaa0a9a30390ee354c4a6e2e6f1ded71
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 17:12:52 +0100

    find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  console.log(people);
+  let ages = people.map(
+    (person) =>
+      (person.yearOfDeath || new Date().getFullYear()) -
+      (person.yearOfBirth)
+  );
+  return people[ages.indexOf(Math.max(...ages))];
 };
 
 // Do not edit below this line
