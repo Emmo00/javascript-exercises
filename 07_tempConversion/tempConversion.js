9fc120c59f6064771085db12ed0db3eca25e9f04
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 15:35:30 +0100

    temp converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let result = ((fahrenheit - 32) * (5 / 9)).toFixed(1);
+  return Number(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = (celsius * (9 / 5) + 32).toFixed(1);
+  return Number(result);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
