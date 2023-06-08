04dd03edf1057455e8ce80bc0ec876802ce6218c
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 15:54:53 +0100

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  return arr.reduce((sum, element) => (sum += element), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (...numbers) {
+  return numbers.reduce((product, element) => product * element, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0) return 1;
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
