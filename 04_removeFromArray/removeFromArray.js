e5bfd366b8e0a7f3c286a22cde5f40f67f5fd320
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-08 14:53:53 +0100

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...number) {
+  number.forEach((num) => {
+    arr = arr.filter((element) => !(element === num));
+  });
+  return arr;
 };
 
 // Do not edit below this line
